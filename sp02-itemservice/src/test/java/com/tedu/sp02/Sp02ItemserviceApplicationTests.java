0117a9e8ff898242f0f36983de34385776c221c3
tarena <tarena@tarena-pc>
2019-11-01 14:20:00 +0800

    lsc2

---
+++ sp02-itemservice/src/test/java/com/tedu/sp02/Sp02ItemserviceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tedu.sp02;
+
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp02ItemserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
